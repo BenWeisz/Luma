394605bd02788366b32d2d00bb86d05d4f0210c9
benweisz <ben.weisz@mail.utoronto.ca>
2021-08-07 01:04:23 -0400

    Convert start / end points of ray to from camera to world post

--- luma/light/ray.py
+++ luma/light/ray.py
@@ -1,9 +1,10 @@
 import numpy as np
 
+from luma.util.geometry import asCoordinate, asHomogeneous
+
 class Ray():
     start: np.array
     end: np.array
-    camera_mat: np.array
 
     def __init__(
         self,
@@ -11,9 +12,8 @@ class Ray():
         end: np.array,
         camera_mat: np.array
     ) -> None:
-        self.camera_mat = camera_mat
-        self.start = start
-        self.end = end
+        self.start = asCoordinate(camera_mat.dot(asHomogeneous(start)))
+        self.end = asCoordinate(camera_mat.dot(asHomogeneous(end)))
     
     def get_point(self, t: float) -> np.array:
         start = self.start
